d67eb63bd0138323200738ba8297f5ecbdde5fe4
Clavier Eliott <clavier.eliott.el@gmail.com>
2022-05-19 16:20:13 +0200

    Update welcome component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,18 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    //path: '',
-    path: 'welcome',
+    path: '',
     loadChildren: () => import('./containers/welcome/welcome.module').then( m => m.WelcomePageModule)
   },
   {
-    //path: 'films-list',
-    path: '',
+    path: 'films-list',
     loadChildren: () => import('./containers/films-list/films-list.module').then( m => m.FilmsListPageModule)
   },
   {
     path: 'films-detail',
     loadChildren: () => import('./containers/films-detail/films-detail.module').then( m => m.FilmsDetailPageModule)
-  },  {
+  },
+  {
     path: 'films-item',
     loadChildren: () => import('./components/films-item/films-item.module').then( m => m.FilmsItemPageModule)
   }
--- src/app/containers/welcome/welcome.page.ts
+++ src/app/containers/welcome/welcome.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomePage implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router) { }
 
   ngOnInit() {
+  }
+
+  public navigate(): void {
+    this.router.navigateByUrl("films-list");
   }
 
 }
