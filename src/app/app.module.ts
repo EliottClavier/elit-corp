40c442d6c5ac16eb7a7d179fe0318ccd48dece4c
Clavier Eliott <clavier.eliott.el@gmail.com>
2022-05-19 13:37:47 +0200

    Fix import pipe truncate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import { TruncatePipe } from './pipes/truncate/truncate.pipe';
+import { TruncatePipe } from "./pipes/truncate/truncate.pipe";
 
 @NgModule({
   declarations: [AppComponent, TruncatePipe],
